123de48d46a849dfd1e00f92c31e73e6dc1b729e
Lucas Azevedo <lhs_azevedo@hotmail.com>
2022-07-09 19:41:59 -0300

    Cleanup main.js

--- src/main.js
+++ src/main.js
@@ -1,28 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import store from './store'
 import { createPinia } from 'pinia'
 import dayjs from 'dayjs'
 import calendar from 'dayjs/plugin/calendar'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
 import relativeTime from 'dayjs/plugin/relativeTime'
-// import * as api from './services/api'
-// import * as server from './services/server.js'
 
-// import '@/styles/app.scss'
 import '@/styles/_global.scss'
-
-// async function init () {
-//   api.init()
-
-//   await server.updateStatus()
-//   server.subscribe()
-
-//   server.updateInfo()
-//   server.updateSettings()
-//   server.updateBackups()
-// }
 
 dayjs.extend(calendar)
 dayjs.extend(customParseFormat)
@@ -34,5 +19,3 @@ const app = createApp(App)
 app.use(pinia)
 app.use(router)
 app.mount('#app')
-
-// init()
